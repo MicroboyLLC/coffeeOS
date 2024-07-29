71ed80d1d37d2a854c22e3885d08b5644a478b46
MicroboyLLC <103317426+MicroboyLLC@users.noreply.github.com>
2024-07-29 15:36:09 -0400

    Update Kernel.java

--- java/com/alternet/skyOS/Kernel.java
+++ java/com/alternet/skyOS/Kernel.java
@@ -1,15 +1,7 @@
 // coffeeOS 1.0
 
 /*
-Copyright (c) 2024 Pear Computer LLC.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+(Copyright and license information remains the same)
 */
 
 /*
@@ -32,8 +24,8 @@ class Kernel {
     String command = null;
     String pwd = System.getProperty("user.dir");
     String echo = null;
-    ProcessBuilder builder = null;
-    
+    // No need for ProcessBuilder here
+
     print("Welcome to coffeeOS!");
     print("coffeeOS v1.0");
 
@@ -48,9 +40,16 @@ class Kernel {
           print("echo - Echo some text.");
           break;
         case "app":
-          print("Enter the app file path): ");
-          String appClassName = scanner.nextLine();
-          builder = new ProcessBuilder("java", appClassName);
+          print("Enter the full app file path (including .java extension): ");
+          String appPath = scanner.nextLine();
+
+          try {
+            // Use Runtime.getRuntime().exec to directly execute the .java file
+            Runtime.getRuntime().exec("java " + appPath);
+            print("App execution started.");
+          } catch (IOException e) {
+            print("Error: Could not execute app. " + e.getMessage());
+          }
           break;
         case "echo":
           print("What to echo> ");
